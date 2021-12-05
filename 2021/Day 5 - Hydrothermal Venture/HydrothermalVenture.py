b5d423d11d3e3242ea2aa09833e1ce63be6f7292
StuartSpiegel <48696059+StuartSpiegel@users.noreply.github.com>
2021-12-05 17:26:06 -0500

    update

--- 2021/Day 5 - Hydrothermal Venture/HydrothermalVenture.py
+++ 2021/Day 5 - Hydrothermal Venture/HydrothermalVenture.py
@@ -33,13 +33,13 @@ lines = [
 
 
 def get_vent_locations_on_line(x1, y1, x2, y2):
-    dx = bool(x2 > x1) - bool(x2 < x1)  # -1|0|1
+    dx = bool(x2 > x1) - bool(x2 < x1)
     dy = bool(y2 > y1) - bool(y2 < y1)
     return [(x1 + n * dx, y1 + n * dy) for n in range(max(abs(x2 - x1), abs(y2 - y1)) + 1)]
 
 
 # Each position is shown as the number of lines that cover that point
-def find_line_overlaps(vent_lines):
+def intersection(vent_lines):
     vent_locations = set()
     overlaps = set()
     for line in vent_lines:
@@ -57,5 +57,5 @@ def is_diagonal(x1, y1, x2, y2):
     return x1 != x2 and y1 != y2
 
 
-print("Part 1:", len(find_line_overlaps([l for l in lines if not is_diagonal(*l)])))
-print("Part 2:", len(find_line_overlaps(lines)))
+print("Part 1:", len(intersection([k for k in lines if not is_diagonal(*k)])))
+print("Part 2:", len(intersection(lines)))
